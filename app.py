b2f8aa869cbbc8d1e89762332f355b72ed02989e
bigsmokio69 <jecs1704cs@gmail.com>
2024-08-09 10:49:16 -0600

    login del sistema y modulo para registrar nuevos medicos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
 import bcrypt
 
@@ -22,8 +22,21 @@ def login():
     rfc=request.form['rfc']
     contra=request.form['contra']
     
-    cursor=mysql.connection.cursor()    
-    return 1
+    cursor=mysql.connection.cursor()
+    cursor.execute('SELECT contrasena FROM medicos WHERE rfc = %s;', (rfc,))
+    usuario = cursor.fetchone()
+    
+    if usuario:
+        hashed_contra = usuario[0].encode('utf-8')
+        
+        if bcrypt.checkpw(contra.encode('utf-8'), hashed_contra):
+            return redirect(url_for('mostrar_dashboard'))
+        else:
+            flash('Contraseña incorrecta')
+            return redirect(url_for('Index'))
+    else:
+        flash('RFC incorrecto')
+        return redirect(url_for('Index'))
 
 @app.route('/mostrar_registro')
 def mostrar_registro():
@@ -31,19 +44,32 @@ def mostrar_registro():
 
 @app.route('/registrar', methods=['POST'])
 def registrar():
-    nombre=request.form['nombre_med']
-    ap=request.form['ap_p']
-    am=request.form['ap_m']
-    rfc=request.form['rfc']
-    tel=request.form['tel']
-    correo=request.form['correo']
-    cedula=request.form['cedula']
-    rol=request.form['rol']
-    contra=request.form['contra']
-    
-    cursor=mysql.connection.cursor()
-    cursor.execute('CALL sp_ins_medico ')
-    return 1
+    if request.method=='POST':
+        nombre=request.form['nombre_med']
+        ap=request.form['ap_p']
+        am=request.form['ap_m']
+        rfc=request.form['rfc']
+        tel=request.form['tel']
+        correo=request.form['correo']
+        cedula=request.form['cedula']
+        rol=request.form['rol']
+        contra=request.form['contra']
+        conf=request.form['confirmar']
+                
+        if contra==conf:
+            hashed_contra = bcrypt.hashpw(contra.encode('utf-8'), bcrypt.gensalt())
+            cursor=mysql.connection.cursor()
+            cursor.execute('CALL sp_ins_medico (%s, %s, %s, %s, %s, %s, %s, %s, %s)', (nombre, ap, am, rfc, tel, correo, cedula, rol, hashed_contra))
+            mysql.connection.commit()
+            flash('Médico agregado correctamente')
+            print('doc agregado con exito')
+        else:
+            flash('Las contraseñas no coinciden')
+    return redirect(url_for('mostrar_registro'))
+
+@app.route('/mostrar_dashboard')
+def mostrar_dashboard():
+    return render_template('dashboard.html')
 
 if __name__=='__main__':#es necesario hacer que main tenga dos guiones bajos
     app.run(port=3000, debug=True)
